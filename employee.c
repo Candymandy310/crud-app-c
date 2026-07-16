b59f55935fc411c64aaa7977a81d471508b9678b
Chandresh V S <vs.chandresh@gmail.com>
2026-07-16 12:47:36 +0530

    added 'modification of records' functionality

--- employee.c
+++ employee.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct employee
 {
-    char name[31];
-    int age;
-    char phno[11];
-    char email[31];
-    float salary;
+    char eid[5];
+    char ename[31];
+    int eage;
+    char ephno[11];
+    char eemail[31];
+    float esalary;
 };
 
 int main()
 {
     struct employee e;
     FILE *fp;
+    FILE *fpt;
     int choice;
     char c = 'Y';
+    char id[5];
+
     printf("===============================================================\n");
     printf("EMPLOYEE RECORD MANAGEMENT SYSTEM USING C");
     while (1)
@@ -36,16 +41,18 @@ int main()
                 while (c == 'Y' || c == 'y')
                 {
                     printf("\n\n---ADDING RECORD---\n");
+                    printf("Enter Employee ID : ");
+                    scanf(" %[^\n]", e.eid);
                     printf("Enter Employee Name (max 30 char) : ");
-                    scanf(" %[^\n]", e.name);
+                    scanf(" %[^\n]", e.ename);
                     printf("Enter Employee Age : ");
-                    scanf("%d", &e.age);
+                    scanf("%d", &e.eage);
                     printf("Enter Employee Phone Number (max 10 digits) : ");
-                    scanf(" %[^\n]", e.phno);
+                    scanf(" %[^\n]", e.ephno);
                     printf("Enter Employee Email (max 30 char) : ");
-                    scanf(" %[^\n]", e.email);
+                    scanf(" %[^\n]", e.eemail);
                     printf("Enter Employee Salary : ");
-                    scanf("%f", &e.salary);
+                    scanf("%f", &e.esalary);
                     printf("\nRecord Collected...\n");
                     fwrite(&e, sizeof(e), 1, fp);
                     printf("Record added...\n");
@@ -54,16 +61,50 @@ int main()
                 }
                 fclose(fp);
                 break;
+            case 2:
+                printf("\n\n---MODIFYING RECORDS---\n\n");
+                fp = fopen("Employee_Rec.txt", "rb");
+                fpt = fopen("temp.txt", "wb");
+                printf("Enter the Employee ID of the record you want to modify : ");
+                scanf(" %[^\n]", id);
+                while (fread(&e, sizeof(e), 1, fp) == 1)
+                {
+                    if (strcmp(e.eid, id) == 0)
+                    {
+                        printf("\nMatching ID found...\n");
+                        printf("\nEnter the NEW details : \n");
+                        printf("Enter Employee Name (max 30 char) : ");
+                        scanf(" %[^\n]", e.ename);
+                        printf("Enter Employee Age : ");
+                        scanf("%d", &e.eage);
+                        printf("Enter Employee Phone Number (max 10 digits) : ");
+                        scanf(" %[^\n]", e.ephno);
+                        printf("Enter Employee Email (max 30 char) : ");
+                        scanf(" %[^\n]", e.eemail);
+                        printf("Enter Employee Salary : ");
+                        scanf("%f", &e.esalary);
+                        printf("\nRecord Collected...\n");
+                        fwrite(&e, sizeof(e), 1, fpt);
+                        printf("Record added...\n");
+                        continue;
+                    }
+                    fwrite(&e, sizeof(e), 1, fpt);
+                }
+                fclose(fp);
+                fclose(fpt);
+                remove("Employee_Rec.txt");
+                rename("temp.txt", "Employee_Rec.txt");
+                break;
             case 4:
                 printf("\n\n---VIEWING RECORDS---\n\n");
                 fp = fopen("Employee_Rec.txt", "rb");
                 rewind(fp);
                 printf("----------------------------------------------------------------------------------------\n");
-                printf("| %25s | %3s | %11s | %25s | %8s |\n", "NAME", "AGE", "PHONE NO", "EMAIL", "SALARY");
+                printf("| %5s | %25s | %3s | %11s | %25s | %8s |\n", "ID", "NAME", "AGE", "PHONE NO", "EMAIL", "SALARY");
                 printf("----------------------------------------------------------------------------------------\n");
                 while (fread(&e, sizeof(e), 1, fp) == 1)
                 {
-                    printf("| %25s | %3d | %11s | %25s | %8.2f |\n", e.name, e.age, e.phno, e.email, e.salary);
+                    printf("| %5s | %25s | %3d | %11s | %25s | %8.2f |\n", e.eid, e.ename, e.eage, e.ephno, e.eemail, e.esalary);
                 }
                 printf("----------------------------------------------------------------------------------------\n");
                 fclose(fp);
