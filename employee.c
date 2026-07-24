3edc1cfe6d3aa6feffb212fbb61dba8c949d1249
Chandresh V S <vs.chandresh@gmail.com>
2026-07-24 17:18:14 +0530

    added searching functionality

--- employee.c
+++ employee.c
@@ -20,19 +20,20 @@ int main()
     int choice;
     char c = 'Y';
     char id[5];
+    int flag = 0;
 
     printf("===============================================================\n");
     printf("EMPLOYEE RECORD MANAGEMENT SYSTEM USING C");
     while (1)
     {
         printf("\n\nFUNCTIONS : \n");
-        printf("1. Add Data\n");
-        printf("2. Modify Data\n");
-        printf("3. Delete Data\n");
-        printf("4. View Data\n");
-        //printf(" Search Data\n");
-        printf("5. Exit\n\n");
-        printf("Enter your choice (1, 2, 3, 4, 5) : ");
+        printf("1. Add Record(s)\n");
+        printf("2. Modify Record\n");
+        printf("3. Delete Record\n");
+        printf("4. View Records\n");
+        printf("5. Search Record\n");
+        printf("6. Exit\n\n");
+        printf("Enter your choice (1, 2, 3, 4, 5, 6) : ");
         scanf("%d", &choice);
 
         switch (choice)
@@ -65,6 +66,7 @@ int main()
             case 2:
                 printf("\n\n---MODIFYING RECORDS---\n\n");
                 fp = fopen("Employee_Rec.txt", "rb");
+                rewind(fp);
                 fpt = fopen("temp.txt", "wb");
                 printf("Enter the Employee ID of the record you want to modify : ");
                 scanf(" %[^\n]", id);
@@ -99,6 +101,7 @@ int main()
             case 3:
                 printf("\n\n---DELETING RECORDS---\n\n");
                 fp = fopen("Employee_Rec.txt", "rb");
+                rewind(fp);
                 fpt = fopen("temp.txt", "wb");
                 printf("Enter the Employee ID of the record you want to delete : ");
                 scanf(" %[^\n]", id);
@@ -132,6 +135,31 @@ int main()
                 fclose(fp);
                 break;
             case 5:
+                printf("\n\n---SEARCHING FOR RECORDS---\n\n");
+                fp = fopen("Employee_Rec.txt", "rb");
+                rewind(fp);
+                printf("Enter the Employee ID of the record you want to search for : ");
+                scanf(" %[^\n]", id);
+                while (fread(&e, sizeof(e), 1, fp) == 1)
+                {
+                    if (strcmp(e.eid, id) == 0)
+                    {
+                        printf("\nMatching ID found...\n\n");
+                        printf("-----------------------------------------------------------------------------------------------\n");
+                        printf("| %5s | %25s | %3s | %11s | %25s | %8s |\n", "ID", "NAME", "AGE", "PHONE NO", "EMAIL", "SALARY");
+                        printf("-----------------------------------------------------------------------------------------------\n");
+                        printf("| %5s | %25s | %3d | %11s | %25s | %8.2f |\n", e.eid, e.ename, e.eage, e.ephno, e.eemail, e.esalary);
+                        printf("-----------------------------------------------------------------------------------------------\n");
+                        flag = 1;
+                    }
+                }
+                if (flag == 0)
+                {
+                    printf("\n NO Matching ID found...\n\n");
+                }
+                fclose(fp);
+                break;
+            case 6:
                 printf("\nExiting the program...\n");
                 exit(0);
             default:
